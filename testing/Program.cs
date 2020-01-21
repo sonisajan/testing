ecf751edf5bbef64a67f7459bbe98e567b6e3394
Sajan Soni <Sajan@cloudthing.com>
2020-01-21 12:04:42 +0530

    Add project files.

---
+++ testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing testing 1, 2, 3");
+        }
+    }
+}
